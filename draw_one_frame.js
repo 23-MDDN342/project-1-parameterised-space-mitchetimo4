7d34b7c8e257627c63e69f8dea7b2406e4dad807
mitchetimo4 <mitchetimo4@myvuw.ac.nz>
2023-03-07 11:08:56 +1300

    Commit 2 (7/3/23)

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -3,8 +3,8 @@ var y=300;
 var a=100;
 var b=100;
 
-function draw_one_frame() {
-  //background(255);
+/*function draw_one_frame() {
+  ///background(255);
   x+=2;
   y+=2;
 	a-=2;
@@ -47,4 +47,114 @@ function drawflower(i,k){
 			stroke(0,(k*0.4)%255,255);
 			arc(0,0,(100+100*cos(k*PI/24))%255,50,0,PI / 20);
 		}
-}
+}*/
+
+function draw_one_frame(cur_frac) {
+	let sun_size = height/8;
+  
+	noStroke();
+	// sky
+	fill(0, 204, 255);
+	rect(0, 0, width, height);
+  
+	// sun
+	fill(255, 255, 0);
+	ellipse(0.25 * width, 0.10 * height, sun_size);
+  
+	// sand
+	fill(223, 173, 96);
+	rect(0, (height/2)+20, width, (height/2)-20);
+  
+	stroke(0);
+	fill(100, 100, 100);
+  
+	let b1_y = 0.55 * height;
+	let b2_y = 0.85 * height;
+  
+	let b1_size = height/12;
+	let b2_size = height/6;
+  
+	let grid_points1 = [
+	 -0.25 * width,
+	  0.0 * width,
+	  0.25 * width,
+	  0.50 * width,
+	  0.75 * width,
+	  1.00 * width
+	]
+  
+	if (debugView) {
+	  stroke(255, 0, 0);
+	  strokeWeight(height/100);
+	  noFill();
+	  for(let i=0; i<grid_points1.length; i++) {
+		rect(grid_points1[i], b1_y, b1_size, 2*b1_size);
+	  }    
+	}
+  
+	/*fill(100, 100, 100);
+	noStroke();
+	for(let i=0; i<grid_points1.length-1; i++) {
+	  let cur_x_pos = map(cur_frac, 0, 1, grid_points1[i], grid_points1[i+1])
+	  rect(cur_x_pos, b1_y, b1_size, 2*b1_size);
+	}*/
+  
+	let grid_points2 = [
+	 -0.40 * width,
+	  0.10 * width,
+	  0.60 * width,
+	  1.10 * width
+	]
+
+	let grid_points3 = [
+		1.10 * width,
+		 0.60 * width,
+		 0.10 * width,
+		 -0.40 * width
+	   ]
+  
+	if(debugView) {
+	  stroke(255, 0, 0);
+	  strokeWeight(height/100);
+	  noFill();
+	  for(let i=0; i<grid_points2.length; i++) {
+		rect(grid_points2[i], b2_y, 2*b2_size, b2_size);
+	  }    
+	}
+
+	/*Pyramid guides*/
+	fill("ffffff");
+	triangle(0, height, width/2, height, width/4, height/5);
+	triangle(width/2, height, width, height, (width/4)*3, height/5);
+
+	/*Draw Slabs*/
+	fill(100, 100, 100);
+	noStroke();
+	for(let i=0; i<grid_points2.length-1; i++) {
+	  let cur_x_pos = map(cur_frac, 0, 1, grid_points2[i], grid_points2[i+1])
+	  rect(cur_x_pos, (height/5)*4, 2.5*b2_size, height/5);
+	}
+
+	fill(100, 100, 100);
+	noStroke();
+	for(let i=0; i<grid_points3.length-1; i++) {
+	  let cur_x_pos = map(cur_frac, 0, 1, grid_points3[i], grid_points3[i+1])
+	  rect(cur_x_pos, (height/5)*3, 2.5*b2_size, height/5);
+	}
+
+	fill(100, 100, 100);
+	noStroke();
+	for(let i=0; i<grid_points2.length-1; i++) {
+	  let cur_x_pos = map(cur_frac, 0, 1, grid_points2[i], grid_points2[i+1])
+	  rect(cur_x_pos, (height/5)*2, 2.5*b2_size, height/5);
+	}
+
+	fill(100, 100, 100);
+	noStroke();
+	for(let i=0; i<grid_points3.length-1; i++) {
+	  let cur_x_pos = map(cur_frac, 0, 1, grid_points3[i], grid_points3[i+1])
+	  rect(cur_x_pos, (height/5), 2.5*b2_size, height/5);
+	}
+  }
+  
+  
