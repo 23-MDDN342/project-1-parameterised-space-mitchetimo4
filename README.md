c44753b656e919a373913955307c47b3558a8a49
mitchetimo4 <mitchetimo4@myvuw.ac.nz>
2023-03-23 11:20:36 +1300

    FINAL COMMIT

--- README.md
+++ README.md
@@ -1,21 +1,10 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300700&assignment_repo_type=AssignmentRepo)
 ### 2023 MDDN342 Project 1: Parameterised Space
-EXAMPLE README 
 
-My design is inspired by fireworks and flowers. I want it to be full of changes, so that every frame can have a fresh feeling.
+Final Commit 23/03/23
 
-In terms of concrete performance, I draw different geometric figures, let them spread out, and overlay their number at the same time, through the time difference to make the whole dynamic background become enthusiastic.
+My design is inspired by the many theories about the creation of the Great Pyramid of Giza. I wanted it to represent at least one of these theories while adhering to the breif. The result of this plan is a fully parameterized gif that can work on any scale and represents the thought that the construction might not have happened how we think it did. 
 
-In terms of color, I made different shapes correspond to different colors, and changed the parameters so that they were different every time they appeared, so that there would be differences between different loops.
+My original plan was to recreate the landscape of Giza with sidescrolling slabs that form pyramids when they line up. I achieved this although during the development I completely changed the way it was presented. Instead of creating a detailed landscape I went for a more simple design and changed the art direction to a more bright and vibrant, cartoonish feel. 
 
-In the initial design, I only added the circle. Through the color change of the circle and the superposition of the movement track, although it has a good effect, I think it is too monotonous.
-
-In the later stages, I added a lot of geometric shapes, which made the color and pattern more variable.
-
-My first sketch is of two pyramids. the idea is to have two of the four layers slide to the right and the other two slide to the left, creating an endless loop of layers coming from each side of the screen. the movement should not be linear so the viewer has a chance to look at the pyramids breifly before they move again. 
-
-14/03/23
-I now have all of the pyramid slabs moving and the right dimensions. Refer to development log for more info. My next step is to add more details but i have been thinking about rethinking the way i want to display what i have. I was originally going to create a scene/setting where the pyramids were in an accurate environment. i have started to rethink this because it feels too busy with the amount of movement. im leaning towards more of an abstract approach but i might backtrack if it doesnt work out. The next step is to experiment with some new designs and then ofc add some more detail. 
-
-21/03/23
-The detail on the slabs has now been added. It is easy to tell that they are pyramids now rather than just triangles being formed over and over. It does however make the animtaion rather busy. If after the next step when i have added the clouds in the lines are making it too busy i am still open to removing them and going with the slabs rather than bricks. 
+Although I strayed away from my orignial art style the idea I wanted to convey is still very much there. I beleive that I followed the brief well and that my final animation is sucessful in terms of what I wanted to create and what the brief wanted me to create. 
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -3,90 +3,24 @@ var y=300;
 var a=100;
 var b=100;
 
-/*function draw_one_frame() {
-  ///background(255);
-  x+=2;
-  y+=2;
-	a-=2;
-	b-=2;
-	strokeWeight(1);
-  translate(width/2, height/2);
-  for(var i=0;i<15;i++){
-	  for(var k=0;k<20;k++){
-		stroke(255,255,255);
-    rotate(PI / 12.0);
-	  fill(255,255-i*10,255-k*10);
-  	line(a%100,b%100,x%300,y%300);
-	  ellipse((x+i*20)%width,(y+k*20)%height,i+4,i+4);
-		drawtriangle((a-i*20)%width,(b-k*20)%height,k/8);
-		rect(x%width, y%height, k+10, k+10);
-		fill(0,i*10,255-k*10);
-		ellipse((x-i*20)%width,(y-k*20)%height,i+3,i+3);
-		rotate(PI / 24.0);
-		fill(255-(i+k)*5,(i+k)*7,i*20);
-		drawtriangle((a+i*20)%width,(b+k*20)%height,k/8);
-		rect(a%width, b%height, k+5, k+5);
-		drawflower(k,x);
-	  }
-  }
-
-}
-
-function drawtriangle(x,y,r){
-	triangle(x, y, x+7*r, y-13.75*r, x+14*r, y);
-}
-
-function drawflower(i,k){
-		if(i%2==1){
-			fill(255,(k*0.4)%255,30);
-			stroke(k%255,255,0);
-			arc(0,0,150+i+150*sin(k*PI/24),150,0,PI / 40);
-		}
-		else{
-			fill(k%255,0,255);
-			stroke(0,(k*0.4)%255,255);
-			arc(0,0,(100+100*cos(k*PI/24))%255,50,0,PI / 20);
-		}
-}*/
-
-//LOAD BACKGROUND IMAGE
-/*let img;
-function preload() {
-	img = loadImage('cloudsPyra.png');
-}
-
-function setup(){
-	image(img, 0, 0);
-}*/
 
 let FirstRun = true
 let img;
 function draw_one_frame(cur_frac) {
 if(FirstRun){
-
 	img = loadImage('cloudsPrya.png');
 	
 	FirstRun = false
-	
 } 
 
-	let sun_size = height/2;
+	let sun_size = height/2; //Set Sun width and height variable
   
 	noStroke();
-	// sky
+	// CREATE SKY
 	fill(0, 204, 255);
 	rect(0, 0, width, height);
   
-
-  
-	// sand
-	/*fill("#ffb323");
-	rect(0, (height/2), width, (height/2));*/
-
-	// top 5th
-	/*fill('#ffffff');
-	rect(0,0,width,height/5);*/
-  
+	
 	stroke(0);
 	fill(100, 100, 100);
   
@@ -95,21 +29,22 @@ if(FirstRun){
   
 	let b1_size = height/12;
 	let b2_size = height/6;
+
 	push()
-	if (width=960){
+	if (width == 960){
 		scale(0.5);
 	image(img, 0, 0);
-	}else if (width = 1920){
+	}else if (width == 1920){
 		scale(1);
 		image(img, 0, 0);
-	}else if (width = 2250){
+	}else if (width == 2250){
 		scale(1.17187);
 		image(img, 0, 0);
 	}
 	pop()
 
-		// sun
-		
+
+	// CREATE SUN	
 	noStroke();
 	fill('#ff5492');
 	ellipse(0.5 * width, 0.02 * height, sun_size);
@@ -120,15 +55,7 @@ if(FirstRun){
 	strokeWeight(height/40);
 	ellipse(0.5 * width, 0.02 * height, sun_size);
 
-	let grid_points1 = [
-	 -0.25 * width,
-	  0.0 * width,
-	  0.25 * width,
-	  0.50 * width,
-	  0.75 * width,
-	  1.00 * width
-	]
-  
+
 	if (debugView) {
 	  stroke(255, 0, 0);
 	  strokeWeight(height/100);
@@ -145,6 +72,7 @@ if(FirstRun){
 	  rect(cur_x_pos, b1_y, b1_size, 2*b1_size);
 	}*/
   
+	//Grid points / keyframes
 	let grid_points2 = [
 	 -0.425 * width,
 	  0.125 * width,
@@ -168,18 +96,19 @@ if(FirstRun){
 	  }    
 	}
 
-	/*Pyramid guides*/
+	/*Pyramid build guides*/
 	/*fill("ffffff");
 	triangle(0, height, width/2, height, width/4, height/5);
 	triangle(width/2, height, width, height, (width/4)*3, height/5);*/
 
-	
+	let slabOuts = height/70; 
+
 	/*Draw Slabs*/
 	fill('#ec7100'); //BOTTOM LAYER
 	noStroke();
 	for(let i=0; i<grid_points2.length-1; i++) {
 	  let cur_x_pos = map(cur_frac, 0, 1, grid_points2[i], grid_points2[i+1])
-	  let base_coord = cur_x_pos-60;
+	  let base_coord = cur_x_pos-(width/15.5);
 	  let width_rect = (4*b2_size);
 	  
 
@@ -195,17 +124,17 @@ if(FirstRun){
 	}
 	noFill(); //outlines for BOTTOM LAYER
 	stroke('#ffffff');
-	strokeWeight(10);
+	strokeWeight(slabOuts);
 	for(let i=0; i<grid_points2.length-1; i++) {
 		let cur_x_pos = map(cur_frac, 0, 1, grid_points2[i], grid_points2[i+1])
-		let base_coord = cur_x_pos-60;
+		let base_coord = cur_x_pos-(width/16.5);
 		let width_rect = (4*b2_size);
 
 		
 	  quad(base_coord,(height/5)*4,base_coord+width_rect,(height/5)*4,base_coord+width_rect+(height/9)+10,(height/5)*5,cur_x_pos-(height/5)-10,height);
-	  line(cur_x_pos+(height/9)*2,(height/5)*4,cur_x_pos+(height/9)*2,(height/5)*5);
-	  line(cur_x_pos+(height/9)*.0005,(height/5)*4,cur_x_pos+(height/9)*0.0005,(height/5)*5);
-	  line(cur_x_pos+(height/9)*4,(height/5)*4,cur_x_pos+(height/9)*4,(height/5)*5);
+	  //line(cur_x_pos+(height/9)*2,(height/5)*4,cur_x_pos+(height/9)*2,(height/5)*5);
+	  //line(cur_x_pos+(height/9)*.0005,(height/5)*4,cur_x_pos+(height/9)*0.0005,(height/5)*5);
+	  //line(cur_x_pos+(height/9)*4,(height/5)*4,cur_x_pos+(height/9)*4,(height/5)*5);
 
 
 	}
@@ -224,15 +153,15 @@ if(FirstRun){
 	}
 	noFill(); //outlines for SECOND FROM BOTTOM LAYER
 	stroke('#ffffff');
-	strokeWeight(10);
+	strokeWeight(slabOuts);
 	for(let i=0; i<grid_points2.length-1; i++) {
 	  let cur_x_pos = map(cur_frac, 0, 1, grid_points3[i], grid_points3[i+1])
 	  base_coord2= cur_x_pos;
 	  let width_rect2 = (2.65*b2_size);
 
 	  quad(base_coord2,(height/5)*3,base_coord2+width_rect2,(height/5)*3,base_coord2+width_rect2+(height/9),(height/5)*4,base_coord2-width_rect2/4,(height/5)*4);
-	  line(cur_x_pos+(height/9),(height/5)*3,cur_x_pos+(height/9),(height/5)*4);
-	  line(cur_x_pos+(height/9)*3,(height/5)*3,cur_x_pos+(height/9)*3,(height/5)*4);
+	  //line(cur_x_pos+(height/9),(height/5)*3,cur_x_pos+(height/9),(height/5)*4);
+	  //line(cur_x_pos+(height/9)*3,(height/5)*3,cur_x_pos+(height/9)*3,(height/5)*4);
 
 
 	}
@@ -248,12 +177,12 @@ if(FirstRun){
 	}
 	noFill(); //outlines for SECOND TO TOP LAYER
 	stroke('#ffffff');
-	strokeWeight(10);
+	strokeWeight(slabOuts);
 	for(let i=0; i<grid_points2.length-1; i++) {
 		let cur_x_pos = map(cur_frac, 0, 1, grid_points2[i], grid_points2[i+1])
 
 		quad(cur_x_pos+(height/3),(height/5)*2,cur_x_pos+(height/9),(height/5)*2,cur_x_pos,(height/5)*3,cur_x_pos+(height/5)*2.21,(height/5)*3);
-		line(cur_x_pos+(height/9)*2,(height/5)*2,cur_x_pos+(height/9)*2,(height/5)*3);
+		//line(cur_x_pos+(height/9)*2,(height/5)*2,cur_x_pos+(height/9)*2,(height/5)*3);
 	}
 
 	fill('#ec7100'); //top layer
@@ -266,7 +195,7 @@ if(FirstRun){
 	}
 	noFill(); //outlines for TOP LAYER
 	stroke('#ffffff');
-	strokeWeight(10);
+	strokeWeight(slabOuts);
 	for(let i=0; i<grid_points2.length-1; i++) {
 		let cur_x_pos = map(cur_frac, 0, 1, grid_points3[i], grid_points3[i+1])
 
